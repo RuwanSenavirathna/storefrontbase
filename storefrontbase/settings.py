a70c9cb18d58217527dead7f81a4e8390c2baaec
Ruwan <rcsenavirathna.com>
2021-09-03 17:18:29 +0530

    Change DB to SQL and migrated

--- storefrontbase/settings.py
+++ storefrontbase/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = 'storefrontbase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefrontbase',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD' : 'Senavi@123'
     }
 }
 
